96c2d71dcd2696a8fc63b1944092d7bd40514336
alcl2000 <amy.555.lewis@gmail.com>
2023-04-25 17:04:10 +0000

    add: get post list method

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+
+
+class PostListView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
