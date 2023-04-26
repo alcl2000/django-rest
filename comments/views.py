269c1f8b38f20d3492d8c3e0527153e9d49a30dd
alcl2000 <amy.555.lewis@gmail.com>
2023-04-26 20:25:23 +0000

    add: comment detail view

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializer import CommentDetailSerializer, CommentSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_class = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
