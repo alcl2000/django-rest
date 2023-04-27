7ad455d7aed017f631dd2353570c5e6bc54e50b9
alcl2000 <amy.555.lewis@gmail.com>
2023-04-27 13:44:35 +0000

    add: follower list view

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import FollowerSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    
