0fb6359715d8b8907285c5e7117edb989552ddf7
alcl2000 <amy.555.lewis@gmail.com>
2023-04-27 12:59:49 +0000

    fix: models line too long error fixed

--- likes/models.py
+++ likes/models.py
@@ -7,7 +7,10 @@ from posts.models import Post
 
 class Like(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='like')
+    post = models.ForeignKey(Post, 
+                             on_delete=models.CASCADE, 
+                             related_name='like'
+                             )
     created_at = models.DateTimeField(auto_now=True)
 
     class Meta:
