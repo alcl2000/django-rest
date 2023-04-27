5d15c92cfe9c454d185fc634e370ef816fa5220c
alcl2000 <amy.555.lewis@gmail.com>
2023-04-27 12:15:47 +0000

    add: like create validation

--- likes/serializer.py
+++ likes/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Like
+from django.db import IntegrityError
 
 
 class LikeSerializer(serializers.ModelSerializer):
@@ -10,3 +11,12 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'post'
         ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
+
