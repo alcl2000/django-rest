7b1ba2d45809685c96e5bf99f5c6d3cd43d5cb0d
alcl2000 <amy.555.lewis@gmail.com>
2023-05-01 17:53:23 +0000

    add: comment filter

--- comments/views.py
+++ comments/views.py
@@ -2,12 +2,19 @@ from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializer import CommentDetailSerializer, CommentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_class = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'post'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
